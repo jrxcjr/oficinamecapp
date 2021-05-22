afd2305d7c65bff9367d0a5c3d862cdc8c3b63f6
José Ricardo Xavier Cavalcanti Júnior <75625584+jrxcjr@users.noreply.github.com>
2021-05-21 22:19:24 -0300

    css fixes

--- components/ServiceList.js
+++ components/ServiceList.js
@@ -26,6 +26,7 @@ export default function ServiceList() {
       .catch(error => console.error(error))
       .finally(() => setLoading(false));
   }, []);
+  //TODO:TRY TO SPLIT THE CONCERNS VIA CONTEXT
 
   return (
     <View style={styles.container}>
@@ -77,6 +78,7 @@ export default function ServiceList() {
               </Text>
               <Text style={styles.modalText}>Value:{clickedData.value}</Text>
               <Button
+                style={styles.modalButton}
                 title="Close modal"
                 onPress={() => setVisibleModal(!visibleModal)}
               />
@@ -97,14 +99,18 @@ const styles = StyleSheet.create({
     flex: 1,
     marginBottom: 20,
     padding: 20,
-    backgroundColor: '#e3e3e3',
+    backgroundColor: '#f7f7f7',
     flexGrow: 1,
   },
   title: {
     fontWeight: 'bold',
     fontSize: 30,
+    alignSelf: 'center',
+    alignContent: 'center',
+    color: '#1f75ff',
   },
   modalBox: {
+    flexDirection: 'column',
     alignContent: 'center',
     margin: 20,
     backgroundColor: 'white',
@@ -118,17 +124,25 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.25,
     shadowRadius: 4,
-    elevation: 5,
+    elevation: 10,
+    backfaceVisibility: 'hidden',
   },
   modalText: {
+    alignContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
     marginBottom: 5,
     padding: 2,
     fontWeight: 'bold',
     fontSize: 18,
   },
+  modalButton: {
+    color: '#1f75ff',
+  },
   list: {
     margin: 10,
     alignContent: 'space-around',
+    elevation: 10,
   },
   listItem: {
     flexDirection: 'column',
@@ -138,10 +152,29 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     padding: 15,
     marginBottom: 10,
-    borderRadius: 20,
-    borderColor: '#aaa',
-    borderWidth: 1.5,
+    borderRadius: 15,
+    borderWidth: 1,
+    borderColor: '#000000',
+    opacity: 0.15,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 2,
+      height: 6,
+    },
+    shadowOpacity: 0.39,
+    shadowRadius: 8.3,
+    elevation: 1,
+    // shadowColor: '#000',
+    // shadowOffset: {
+    //   width: 0,
+    //   height: 3,
+    // },
+    // shadowOpacity: 0.29,
+    // shadowRadius: 4.65,
+
+    // elevation: 7,
     margin: 18,
     fontSize: 17,
   },
 });
+//TODO:card shadow, modal to center,
